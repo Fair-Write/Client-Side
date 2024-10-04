f049e66e7f379c756205e3e5d6f50b07494068bc
LueKely <luekely43110@gmail.com>
2024-10-04 22:39:16 +0800

    Added: Drag and drop basic functionalites

---
+++ src/lib/features/document-scan/presentation/DragAndDrop.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  // flow
+  // drag file to zone
+  // on drag check if file is a valid type
+  // txt/pdf/document
+  // if valid
+  // return output string i guess
+
+  import { dropHandler, dragOverHandler } from "../use-case/extractDocument";
+</script>
+
+<div
+  class="drop-container"
+  role="button"
+  aria-label="Upload files"
+  aria-describedby="upload-instructions"
+  tabindex="0"
+  on:drop={(e) => dropHandler(e)}
+  on:dragover={(e) => dragOverHandler(e)}
+>
+  <p>Drag Your Files Here <br /> OR/</p>
+
+  <button>Open File Here</button>
+</div>
+
+<style>
+  .drop-container {
+    border: solid 5px blue;
+    width: 100px;
+    height: 100px;
+  }
+</style>
---
+++ src/lib/features/document-scan/use-case/extractDocument.ts
@@ -0,0 +1,31 @@
+function getSuffix(fileName: string) {
+  fileName.at(fileName.length - 4);
+}
+
+export function dropHandler(e: DragEvent) {
+  console.log("File has been dropped");
+  e.preventDefault();
+
+  if (e.dataTransfer!.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    [...e.dataTransfer!.items].forEach((item, i) => {
+      // If dropped items aren't files, reject them
+      if (item.kind === "file") {
+        const file = item.getAsFile();
+        console.log(`… file[${i}].name = ${file!.name}`);
+      }
+    });
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    [...e.dataTransfer!.files].forEach((file, i) => {
+      console.log(`… file[${i}].name = ${file.name}`);
+    });
+  }
+}
+
+export function dragOverHandler(ev: Event) {
+  console.log("File(s) in drop zone");
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+}
