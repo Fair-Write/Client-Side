af6cc94d71d73fd252f56f6d31a444396542ccb0
LueKely <luekely43110@gmail.com>
2024-10-09 09:59:31 +0800

    Feat: integrated file types to text conversion

--- src/lib/features/document-scan/presentation/DragAndDrop.svelte
+++ src/lib/features/document-scan/presentation/DragAndDrop.svelte
@@ -12,35 +12,30 @@
     convertDragToFile,
     convertInputToFile,
   } from "../use-case/extractDocument";
-  import { getTextFromPdf } from "../use-case/pdfToText";
 
   let fileDocument: null | File = null;
-
-  onMount(() => {
-    console.log(getTextFromPdf("/LoremText.pdf"));
-  });
 </script>
 
 <div>
-  <form class="drop-container">
+  <form class="scan-container">
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div
-      role="button"
-      aria-label="Upload files"
-      aria-describedby="upload-instructions"
-      tabindex="0"
+      class="drop-wrapper"
       on:drop={(e) => {
+        e.preventDefault();
         dropHandler(convertDragToFile(e));
       }}
       on:dragover={(e) => dragOverHandler(e)}
-    ></div>
-    <div>
-      <label for="File_Drop">Drag your File Here</label>
-      <input
-        on:change={(e) => dropHandler(convertInputToFile(e))}
-        id="File_Drop"
-        accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        type="file"
-      />
+    >
+      <div>
+        <label for="File_Drop">Drag your File Here</label>
+        <input
+          on:change={(e) => dropHandler(convertInputToFile(e))}
+          id="File_Drop"
+          accept=".pdf,.doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+          type="file"
+        />
+      </div>
     </div>
 
     <button type="submit">Analyze</button>
@@ -48,9 +43,15 @@
 </div>
 
 <style>
-  .drop-container {
+  .scan-container {
     border: solid 5px blue;
     width: 500px;
     height: 500px;
+  }
+
+  .drop-wrapper {
+    width: 200px;
+    height: 200px;
+    background-color: rgb(184, 167, 167);
   }
 </style>
---
+++ src/lib/features/document-scan/types/pdfjs-dist.d.ts
@@ -0,0 +1,5 @@
+// types/pdfjs-dist.d.ts
+declare module "pdfjs-dist/build/pdf.worker.mjs" {
+  const worker: unknown; // You can improve the types later if necessary
+  export default worker;
+}
