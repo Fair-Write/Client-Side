eec4df2d9d88ab42d01f3f7ac1e9c9a3a082779c
LueKely <luekely43110@gmail.com>
2024-10-22 13:11:14 +0800

    awdad

--- src/lib/features/rich-text-editor/presentation/RichTextEditor.svelte
+++ src/lib/features/rich-text-editor/presentation/RichTextEditor.svelte
@@ -51,7 +51,7 @@
         editor.chain().focus().toggleHeading({ level: 3 }).run();
         break;
       case "p":
-        editor.chain().focus().toggleParagraph().run();
+        editor.chain().focus().setParagraph().run();
         break;
       case "bold":
         editor.chain().focus().toggleBold().run();
