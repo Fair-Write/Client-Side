91a3a4a277beb6cbf5efad12d6e85cf1fc147372
LueKely <luekely43110@gmail.com>
2024-10-23 12:35:02 +0800

    d

--- src/lib/features/rich-text-editor/presentation/RichTextEditor.svelte
+++ src/lib/features/rich-text-editor/presentation/RichTextEditor.svelte
@@ -129,7 +129,7 @@
 	<!-- ProseMirror editor container -->
 	<div
 		bind:this={editorContainer}
-		class="prose prose-lg w-[700px] flex-1 bg-border"
+		class="prose prose-lg w-[700px] flex-1 bg-stone-50"
 		id="editor"
 	></div>
 </section>
