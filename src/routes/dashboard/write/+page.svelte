0998e5f329d481d7f6db775baef5602fb3775766
LueKely <luekely43110@gmail.com>
2024-10-31 10:22:19 +0800

    Feat: added skeleton

--- src/routes/dashboard/write/+page.svelte
+++ src/routes/dashboard/write/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
-  import RichTextEditor from "$lib/features/rich-text-editor/presentation/RichTextEditor.svelte";
+	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 </script>
 
-<RichTextEditor></RichTextEditor>
+<!--{#await import('$lib/features/rich-text-editor/presentation/RichTextEditor.svelte')}-->
+<section class="flex flex-1 flex-col items-center bg-stone-200">
+	<Skeleton class=" w-full h-[55px] rounded-none" />
+	<Skeleton
+		class="flex-1 w-[700px] rounded-none"
+	></Skeleton>
+</section>
+<!--	>-->
+<!--{:then c}-->
+<!--	<svelte:component this={c.default} />-->
+<!--{/await}-->
+
+
