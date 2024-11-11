8ad277e3a14054d7afbb8fce4f7b3945183917f0
LueKely <luekely43110@gmail.com>
2024-11-11 11:06:11 +0800

    Build: migrated shadcn svelte to svelte 5

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,59 +1,54 @@
 <script lang="ts">
-  import { cn } from "$lib/utils";
-  import { page } from "$app/stores";
-  import SuggestionBot from "$lib/features/suggestion-bot/presentation/suggestionBot.svelte";
-
-
+	import { cn } from '$lib/utils';
+	import { page } from '$app/stores';
+	import SuggestionBot from '$lib/features/suggestion-bot/presentation/suggestionBot.svelte';
 </script>
 
-<header
-  class="flex items-end justify-between border-b border-solid border-stone-300 bg-stone-50 p-3 shadow-sm"
->
-  <h1 class="text-2xl">Fair Write</h1>
-  <div>icon things</div>
-</header>
 <div class="flex flex-1 bg-stone-100">
-  <nav
-    class="flex w-64 flex-col justify-between gap-2 border-r border-solid border-stone-300"
-  >
-    <!-- wrapper -->
-    <div class="flex flex-col gap-2 px-3">
-      <!-- write and scan -->
-      <span
-        class="flex h-14 items-center justify-start border-b border-dashed border-stone-400"
-      >
-        <h3 class=" text-xl font-semibold text-stone-400">Features</h3>
-      </span>
+	<div class="flex w-64 flex-col justify-between gap-2 border-r border-solid border-stone-300">
+		<!-- wrapper -->
 
-      <a
-        href="/dashboard/write"
-        class={cn(
-          "rounded-lg border border-transparent p-2 text-xl ",
-          " hover:bg-stone-200 ",
-          $page.url.pathname === "/dashboard/write" &&
-            "border-solid border-stone-300 bg-stone-50 shadow-md hover:bg-stone-50",
-        )}
-      >
-        Write
-      </a>
-      <a
-        href="/dashboard/scan"
-        class={cn(
-          "rounded-lg border border-transparent  p-2 text-xl",
-          " hover:bg-stone-200 ",
-          $page.url.pathname === "/dashboard/scan" &&
-            "border-solid border-stone-300 bg-stone-50 shadow-md hover:bg-stone-50 ",
-        )}>Scan</a
-      >
-    </div>
+		<div class="flex flex-col gap-1 *:px-2">
 
-    <!-- info -->
-    <a href="/dashboard/info" class="border-t border-stone-300 p-2 text-xl"
-      >Info</a
-    >
-  </nav>
-  <main class="flex flex-1 bg-stone-200">
-    <slot></slot>
-    <SuggestionBot></SuggestionBot>
-  </main>
+			<div class="h-14 border-stone-300 border-b border-solid bg-stone-50 flex items-center justify-start ">
+				<h1 class="text-2xl font-Leckerli-One ">Fair Write</h1>
+			</div>
+
+			<div class="flex h-14 items-center justify-start border-b border-dashed border-stone-400 mb-5">
+				<h3 class=" text-xl font-semibold text-stone-400">Features</h3>
+			</div>
+			<!-- editor and scan -->
+      <nav class="flex flex-col gap-2 *:px-2">
+        <a
+          href="/dashboard/editor"
+          class={cn(
+					'rounded-lg border border-transparent p-2 text-xl ',
+					' hover:bg-stone-200 ',
+					$page.url.pathname === '/dashboard/editor' &&
+						'border-solid border-stone-300 bg-stone-50 shadow-md hover:bg-stone-50'
+				)}
+        >
+          Editor
+        </a>
+        <a
+          href="/dashboard/scan"
+          class={cn(
+					'rounded-lg border border-transparent  p-2 text-xl',
+					' hover:bg-stone-200 ',
+					$page.url.pathname === '/dashboard/scan' &&
+						'border-solid border-stone-300 bg-stone-50 shadow-md hover:bg-stone-50 '
+				)}>Scan</a
+        >
+
+      </nav>
+
+		</div>
+
+		<!-- info -->
+		<a href="/dashboard/info" class="border-t border-stone-300 p-2 text-xl">Info</a>
+	</div>
+	<main class="flex flex-1 bg-stone-200">
+		<slot></slot>
+		<SuggestionBot></SuggestionBot>
+	</main>
 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import { fontFamily } from 'tailwindcss/defaultTheme';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
-
+import tailwindcssAnimate from 'tailwindcss-animate';
 const config: Config = {
 	darkMode: ['class'],
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	safelist: ['dark'],
-	plugins: [typography],
+	plugins: [typography, tailwindcssAnimate],
 	theme: {
 		container: {
 			center: true,
@@ -57,7 +57,28 @@ const config: Config = {
 				sm: 'calc(var(--radius) - 4px)'
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+				sans: ['Open Sans', ...fontFamily.sans],
+				'Leckerli-One': 'Leckerli One '
+			},
+
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' }
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' }
+				},
+				'caret-blink': {
+					'0%,70%,100%': { opacity: '1' },
+					'20%,50%': { opacity: '0' }
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'caret-blink': 'caret-blink 1.25s ease-out infinite'
 			}
 		}
 	}
