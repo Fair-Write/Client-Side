648b0ddbe93cd9ca652672a73c84c78c9b58164a
LueKely <luekely43110@gmail.com>
2024-11-16 17:07:54 +0800

    init carousel

---
+++ src/lib/features/suggestion-bot/presentation/StepInfographic.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Progress } from '$lib/components/ui/progress';
+	import { progressStore } from '$lib/stores/progressStore';
+</script>
+
+<div class="mb-5 flex w-full flex-col items-center justify-start gap-2">
+	<div class="relative flex w-full items-center justify-between">
+		<span
+			class="material-symbols-outlined s26 z-10 rounded-full border-2 border-solid border-blue-500 bg-blue-100 p-2 text-blue-500"
+			>edit
+		</span>
+
+		<span
+			class="material-symbols-outlined s26 z-10 rounded-full border-2 border-solid border-stone-500 bg-stone-100 p-2 text-stone-500"
+			>abc
+		</span>
+
+		<span
+			class="material-symbols-outlined s26 r z-10 rounded-full border-2 border-solid border-stone-500 bg-stone-100 p-2 text-stone-500"
+			>manage_search
+		</span>
+
+		<Progress value={$progressStore} class="absolute z-0 h-1 text-red-500"></Progress>
+	</div>
+	<div class=" flex w-full items-center justify-between px-1">
+		<p>Write</p>
+		<p>Grammar</p>
+		<p>GFL</p>
+	</div>
+</div>
--- src/lib/features/suggestion-bot/presentation/SuggestionTab.svelte
+++ src/lib/features/suggestion-bot/presentation/SuggestionTab.svelte
@@ -3,6 +3,17 @@
 	// import { Button } from '$lib/components/ui/button';
 	// import { replaceStore } from '$lib/stores/lintingStore';
 	import StepWrite from '$lib/features/suggestion-bot/presentation/StepWrite.svelte';
+	import StepInfographic from '$lib/features/suggestion-bot/presentation/StepInfographic.svelte';
+	import * as Carousel from '$lib/components/ui/carousel/index.js';
+	import StepGrammar from '$lib/features/suggestion-bot/presentation/StepGrammar.svelte';
+	import StepGLF from '$lib/features/suggestion-bot/presentation/StepGLF.svelte';
+	import Analytics from '$lib/features/suggestion-bot/presentation/Analytics.svelte';
+
+	import { type CarouselAPI } from '$lib/components/ui/carousel/context.js';
+	let api = $state<CarouselAPI>();
+
+
+
 </script>
 
 <!--TODO:-->
@@ -20,33 +31,50 @@
 	</div>
 
 	<div class="p-5">
-		<StepWrite></StepWrite>
-<!--		<Button-->
-<!--			onclick={() => {-->
-<!--				$replaceStore = $aiSuggestions.map((suggestions) =>-->
-<!--					omitObject(suggestions, 'analysis', 'correctionType')-->
-<!--				);-->
-<!--			}}-->
-<!--		>-->
-<!--			REPLACE ALL-->
-<!--		</Button>-->
-<!--		{#each $aiSuggestions as suggestion}-->
-<!--			<div class="my-5 flex flex-col gap-2">-->
-<!--				<p>wrong phrase: {suggestion.wrongPhrase}</p>-->
-<!--				<p>correct phrase: {suggestion.correctPhrase}</p>-->
-<!--				<Button-->
-<!--					onclick={() => {-->
-<!--						$replaceStore = [omitObject(suggestion, 'analysis', 'correctionType')];-->
-<!--					}}>replace/Button-->
-<!--				>-->
-<!--			</div>-->
-<!--		{/each}-->
+		<StepInfographic></StepInfographic>
+
+		<Carousel.Root setApi={(emblaApi) => (api = emblaApi)}>
+			<Carousel.Content>
+				<Carousel.Item><StepWrite></StepWrite></Carousel.Item>
+				<Carousel.Item><StepGrammar></StepGrammar></Carousel.Item>
+				<Carousel.Item><StepGLF></StepGLF></Carousel.Item>
+				<Carousel.Item><Analytics></Analytics></Carousel.Item>
+			</Carousel.Content>
+			<Carousel.Previous />
+
+			<button
+				onclick={() => {
+				if (api) {
+					api.scrollNext();
+				}
+			}}
+			>
+				NEXT</button
+			>
 
 
+		</Carousel.Root>
+
+
+		<!--		<Button-->
+		<!--			onclick={() => {-->
+		<!--				$replaceStore = $aiSuggestions.map((suggestions) =>-->
+		<!--					omitObject(suggestions, 'analysis', 'correctionType')-->
+		<!--				);-->
+		<!--			}}-->
+		<!--		>-->
+		<!--			REPLACE ALL-->
+		<!--		</Button>-->
+		<!--		{#each $aiSuggestions as suggestion}-->
+		<!--			<div class="my-5 flex flex-col gap-2">-->
+		<!--				<p>wrong phrase: {suggestion.wrongPhrase}</p>-->
+		<!--				<p>correct phrase: {suggestion.correctPhrase}</p>-->
+		<!--				<Button-->
+		<!--					onclick={() => {-->
+		<!--						$replaceStore = [omitObject(suggestion, 'analysis', 'correctionType')];-->
+		<!--					}}>replace/Button-->
+		<!--				>-->
+		<!--			</div>-->
+		<!--		{/each}-->
 	</div>
-
-
-
-
-
 </section>
