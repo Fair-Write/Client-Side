f444f1db5b5f2fb47e270e69f5fd2f26d0d88696
LueKely <luekely43110@gmail.com>
2024-11-21 21:42:53 +0800

    WE CAN FINISH THIS

--- src/lib/features/suggestion-bot/presentation/Analytics.svelte
+++ src/lib/features/suggestion-bot/presentation/Analytics.svelte
@@ -1,1 +1,35 @@
-<p>i am analytics</p>
+<script lang="ts">
+
+	import * as Card from "$lib/components/ui/card/index.js";
+	import { Button } from '$lib/components/ui/button';
+
+</script>
+
+<div class="container">
+	<div class="gauge-a"></div>
+	<div class="gauge-b"></div>
+	<div class="gauge-c"></div>
+	<div class="gauge-data">
+		<h1 id="percent">0%</h1>
+	</div>
+
+</div>
+
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title>Useful Articles</Card.Title>
+	</Card.Header>
+	<Card.Content>
+		<a target="_blank" href="www.google.com">how we detect gender fair language</a>
+		<a target="_blank" href="www.google.com">What is gender fair language</a>
+	</Card.Content>
+
+</Card.Root>
+<Button>Start All Over Again</Button>
+<Button>Export As</Button>
+
+
+<style >
+
+</style>
