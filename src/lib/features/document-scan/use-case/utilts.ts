6fd8d9a4ff1a1e019caed076609bd5ddd7cdfe7e
LueKely <luekely43110@gmail.com>
2024-11-24 21:07:44 +0800

    LES FUCKING GO GUAMNA

--- src/lib/features/document-scan/use-case/utilts.ts
+++ src/lib/features/document-scan/use-case/utilts.ts
@@ -1,22 +1,20 @@
 // Utils function
-export function getSuffix(fileName: string): 'pdf'| 'docx'|  'jpeg'| 'png' {
-	const lookUpTable = ['pdf', 'docx',  'jpeg', 'png'] as const;
-  let suffix: typeof lookUpTable[number] | undefined;
+export function getSuffix(fileName: string): 'pdf' | 'docx' | 'jpeg' | 'png' {
+	const lookUpTable = ['pdf', 'docx', 'jpeg', 'png'] as const;
+	let suffix: (typeof lookUpTable)[number] | undefined;
 
-  // Checks if the fileName has the correct suffix
-  lookUpTable.forEach((format) => {
-    if (fileName.endsWith(`.${format}`)) { // Use `endsWith` for stricter matching
-      suffix = format;
-    }
-  });
+	// Checks if the fileName has the correct suffix
+	lookUpTable.forEach((format) => {
+		if (fileName.endsWith(`.${format}`)) {
+			// Use `endsWith` for stricter matching
+			suffix = format;
+		}
+	});
 
-  // Validate suffix
-  if (!suffix) {
-    throw new Error('Invalid File Type');
-  }
+	// Validate suffix
+	if (!suffix) {
+		throw new Error('Invalid File Type');
+	}
 
-  return suffix;
-
+	return suffix;
 }
-
-
