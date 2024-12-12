ad07ee67b4fb31782f6c88fae57fc838148c340a
LueKely <luekely43110@gmail.com>
2024-12-12 09:36:25 +0800

    Merge pull request #59
    * not working
    
    * Changed to pdfjs instead of pdfjs extract

--- src/routes/api/extract/pdf/+server.ts
+++ src/routes/api/extract/pdf/+server.ts
@@ -1,4 +1,13 @@
-import { PDFExtract, type PDFExtractOptions, type PDFExtractResult } from 'pdf.js-extract';
+import * as PDFJS from 'pdfjs-dist';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/ban-ts-comment
+// @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as pdfWorker from 'pdfjs-dist/build/pdf.worker.mjs';
+
+// Set worker source
+PDFJS.GlobalWorkerOptions.workerSrc = import.meta.url + 'pdfjs-dist/build/pdf.worker.mjs';
+
 import { type RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
@@ -15,27 +24,38 @@ export const POST: RequestHandler = async ({ request }) => {
 		});
 	}
 
-	const pdfExtract = new PDFExtract();
-	const options: PDFExtractOptions = {}; // specify options as needed
-	const arrayBuffer = await file.arrayBuffer();
-	const bufferDocx = Buffer.from(arrayBuffer);
+	const getPageText = async (pdf: PDFJS.PDFDocument, pageNo: number) => {
+		const page = await pdf.getPage(pageNo);
+		const tokenizedText = await page.getTextContent();
+
+		return tokenizedText.items.map((token) => token.str).join('');
+	};
+
+	const getPDFText = async (source: ArrayBuffer): Promise<string> => {
+		// Use configuration options when loading document
+		const pdf = await PDFJS.getDocument({
+			data: new Uint8Array(source),
+			disableTextLayer: true, // Use configuration option here
+			isEvalSupported: false,
+			useSystemFonts: true
+		}).promise;
+
+		const maxPages = pdf.numPages;
+		const pageTextPromises = [];
+		for (let pageNo = 1; pageNo <= maxPages; pageNo += 1) {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-expect-error
+			pageTextPromises.push(getPageText(pdf, pageNo));
+		}
+		const pageTexts = await Promise.all(pageTextPromises);
+		return pageTexts.join(' ');
+	};
 
 	try {
-		const data: PDFExtractResult = await new Promise((resolve, reject) => {
-			pdfExtract.extractBuffer(bufferDocx, options, (err, data) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(data as PDFExtractResult);
-				}
-			});
-		});
+		const pdfBuffer = await file.arrayBuffer();
+		const results = await getPDFText(pdfBuffer);
 
-		const allPagesContent = await data.pages
-			.map((page) => page.content.reduce((acc, curr) => acc + curr.str, ''))
-			.join('\n');
-
-		return new Response(JSON.stringify({ data: allPagesContent }), { status: 200 });
+		return new Response(JSON.stringify({ data: results }), { status: 200 });
 	} catch (err) {
 		return new Response(JSON.stringify({ error: String(err) }), { status: 500 });
 	}
