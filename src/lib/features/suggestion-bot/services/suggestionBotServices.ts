55a6423b931268e13baefdaef2bf2ab670f5ef39
Lue Kely <luekely43110@gmail.com>
2025-01-15 10:55:36 +0800

    looks like there's no problem

--- src/lib/features/suggestion-bot/services/suggestionBotServices.ts
+++ src/lib/features/suggestion-bot/services/suggestionBotServices.ts
@@ -7,16 +7,15 @@ import { GLFScore } from '$lib/stores/omegaLOL';
 import { revisedTextStore } from '$lib/stores/revisedTextStore';
 // import axiosInstance from '../../../../service/axios';
 import { toast } from 'svelte-sonner';
+
+// this may be the culprit
 function isStringOrArrayOfStrings(value: string | string[]) {
-	if (typeof value === 'string') {
-		return value; // It's a string
-	}
-
 	if (Array.isArray(value)) {
-		return value[0]; // It's an array of strings
+		// If it's an array, use the first element
+		return value[0];
 	}
-
-	return false; // It's neither a string nor an array of strings
+	// Otherwise, it's a string, return it directly
+	return value;
 }
 
 // const checkUrl = () => {
@@ -36,10 +35,9 @@ export async function grammarCheckService(nextSlide: () => void) {
 	// 		replacement: 'firefighter',
 	// 		correctionType: 'grammar',
 	// 		rationale: 'lorem ipsum somethign something',
-	// 		offSet: 24,
-	// 		endSet: 31,
-	// 		indexReplacement: 5,
-	// 		originalCharacterEndset: 31
+	// 		offSet: 23,
+	// 		endSet: 30,
+	// 		indexReplacement: 5
 	// 	}
 	// ]);
 
@@ -68,7 +66,6 @@ export async function grammarCheckService(nextSlide: () => void) {
 					original_text: string;
 					message: string;
 					replacements: string[];
-
 				}) => ({
 					indexReplacement: correction.word_index,
 					originalText: correction.original_text,
@@ -77,8 +74,7 @@ export async function grammarCheckService(nextSlide: () => void) {
 					replacement: isStringOrArrayOfStrings(correction.replacements),
 					correctionType: 'grammar',
 					message: correction.message,
-					rational: '',
-
+					rational: ''
 				})
 			);
 
@@ -98,7 +94,7 @@ export async function grammarCheckService(nextSlide: () => void) {
 export async function glfCheckService(nextSlide: () => void) {
 	// For Testing
 	// setTimeout(() => {
-	// 	$aiSuggestions = [
+	// 	aiSuggestions.set([
 	// 		{
 	// 			message: 'Change to firefighter',
 	// 			originalText: 'firemen',
@@ -108,12 +104,12 @@ export async function glfCheckService(nextSlide: () => void) {
 	// 			endSet: 29,
 	// 			indexReplacement: 4,
 	// 			rationale: 'lorem ipsum somethign something'
-	// originalCharacterEndset:31
 	// 		}
-	// 	];
+	// 	]);
 	// }, 500);
 	// nextSlide();
-	// $progressStore = 100;
+	// progressStore.set(100);
+
 	try {
 		const post = await fetch('http://127.0.0.1:8080/gfl', {
 			method: 'POST',
@@ -125,11 +121,6 @@ export async function glfCheckService(nextSlide: () => void) {
 			})
 		});
 
-		// const post = axiosInstance.post(`${checkUrl()}/grammar`, {
-		// 	body: JSON.stringify({ prompt: get(textContent) })
-		// });
-
-		// const data = await post;
 		const data = await post.json();
 		console.log(data);
 		await revisedTextStore.set(data.revised_text as string);
@@ -142,7 +133,6 @@ export async function glfCheckService(nextSlide: () => void) {
 					original_text: string;
 					message: string;
 					replacements: string[] | string;
-
 				}) => ({
 					indexReplacement: correction.word_index,
 					originalText: correction.original_text,
@@ -151,8 +141,7 @@ export async function glfCheckService(nextSlide: () => void) {
 					replacement: isStringOrArrayOfStrings(correction.replacements),
 					correctionType: 'gfl',
 					message: 'Gender Fair Language',
-					rational: '',
-
+					rational: ''
 				})
 			);
 
