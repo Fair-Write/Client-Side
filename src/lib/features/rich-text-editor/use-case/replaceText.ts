c8e7dd2edc8d74baaf6f271ed24658468ea5b6a9
Lue Kely <luekely43110@gmail.com>
2025-03-02 11:59:55 +0800

    yeah

--- src/lib/features/rich-text-editor/use-case/replaceText.ts
+++ src/lib/features/rich-text-editor/use-case/replaceText.ts
@@ -2,8 +2,6 @@ import { EditorState, Transaction } from 'prosemirror-state';
 import { Node } from 'prosemirror-model';
 import type { TSuggestion } from '$lib/features/suggestion-bot/entities/suggestions';
 
-
-// todo: rework this what you have to do is find the node that contains the exact word
 import { getWordIndices } from '$lib/utils';
 import { textContent } from '$lib/stores/textFromEditorStore';
 
@@ -18,34 +16,26 @@ export function replaceWordInDocument(
 	const { doc } = editorState;
 	const transaction = editorState.tr;
 
-	// Array to store replacements
-
 	// Traverse each text node in the document
 	doc.descendants((node: Node, pos: number) => {
 		if (node.isText && node.textContent) {
-			// const nodeSplit = node.text!.split(' ')[indexReplacement];
-			// const match = getWordIndices(node.text as string, indexReplacement);
-			// if (match !== null && nodeSplit == originalText) {
-			transaction.replaceWith(
-				// match.start + 1,
-				// match.end + 2,
-				offSet + 1,
-				endSet + 1,
-				editorState.schema.text(replacement)
-			);
-			// }
+			const nodeText = node.text!;
+			const start = offSet + 1;
+			const end = endSet + 1;
+
+			if (nodeText.slice(start, end) === originalText) {
+				const marks = node.marks;
+				const newTextNode = editorState.schema.text(replacement, marks);
+				transaction.replaceWith(pos + start, pos + end, newTextNode);
+			}
 		}
 	});
-
-
 
 	// Apply the transaction if there were changes
 	if (transaction.docChanged) {
 		dispatch(transaction);
-		console.log("REPLACE" + transaction.doc.textContent);
-		textContent.set(transaction.doc.textContent)
-		console.log("After" + get(textContent));
-
-
+		console.log('REPLACE' + transaction.doc.textContent);
+		textContent.set(transaction.doc.textContent);
+		console.log('After' + get(textContent));
 	}
 }
