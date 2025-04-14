ca8e8b61721ae9a28811b271fac0dbe2943cfe4e
Lue Kely <luekely43110@gmail.com>
2025-04-15 07:44:40 +0800

    added pnpm i guess

--- src/lib/stores/textFromEditorStore.ts
+++ src/lib/stores/textFromEditorStore.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 //
-export const textContent = writable('<p>LUE IS AWESOME</p>');
+export const textContent = writable('');
 
 // export const textContent = writable('');
 export const textTitle = writable('Untitled_1');
