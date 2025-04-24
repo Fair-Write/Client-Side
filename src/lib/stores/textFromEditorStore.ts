a709363918d68d400de9c27ad5a05ed305a19b35
Lue Kely <luekely43110@gmail.com>
2025-04-24 10:41:17 +0800

    added this

--- src/lib/stores/textFromEditorStore.ts
+++ src/lib/stores/textFromEditorStore.ts
@@ -1,7 +1,10 @@
 import { writable } from 'svelte/store';
 //
 export const textContent = writable('');
-
+// sample edit
+// export const textContent = writable(
+// 	'In times of emergency, firemen is the brave ones who risk their lives to save others, while policemen work tirelessly to enforce law and order on our streets. These men are just naturally inclined towards such roles, given there physical strength and cowrage. Firemen and policemen undergo rigorous training that prepare them for the challenging situations they face everyday, showing that some jobs simply fit men better. Women might work as policewomen or lady firefighters, but its often a tough fit for them as compared to their male colleagues.'
+// );
 // export const textContent = writable('');
 export const textTitle = writable('Untitled_1');
 export const textContentHTML = writable('');
